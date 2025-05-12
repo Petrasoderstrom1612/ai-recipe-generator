86bc8cf47ece569eac73d3a34496bf7f2f9c165b
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-05-12 11:34:48 +0200

    added Main.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Header from './Header'
-
+import Main from './Main'
 
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Main/>
     </>
   )
 }
